03e86693076077ccf0b2571f2b198e15599e7009
ermia <afsharianarmia@gmail.com>
2019-11-06 20:26:01 -0800

    ball movement

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -5,11 +5,23 @@ export default class Ball {
       this.radius = radius;
       this.x = x;
       this.y = y;
+      this.boardWidth = boardWidth;
+      this.boardHeight = boardHeight;
       this.direction = 1;
+      this.reset();
+    }
+    ballMove(){
+        this.x+= this.vx;
+        this.y+= this.vy;
     }
     reset() {
         this.x = this.boardWidth / 2;
         this.y = this.boardHeight / 2;
+        this.vy=0;
+        while (this.vy===0) {
+            this.vy=Math.floor(Math.random() * 10 -5)
+        }
+        this.vx=this.direction * (6 - Math.abs(this.vy));
       }
     render(svg){
         const circle = document.createElementNS(SVG_NS, "circle");
@@ -18,7 +30,7 @@ export default class Ball {
         circle.setAttributeNS(null, "cy", this.y);
         circle.setAttributeNS(null, "fill", "white");
         svg.appendChild(circle);
-
+        this.ballMove();
 
     }
   }
